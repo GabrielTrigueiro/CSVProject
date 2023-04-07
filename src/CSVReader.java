08a01b4780389d737e3ee2abecaa612829a80a05
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-04-07 13:44:34 -0300

    lendo linha por linha de printando

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -1,15 +1,22 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class CSVReader {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
+        //path do arquivo
         String path = "C:/Users/gabri/OneDrive/Documentos/Trabalho/ProjetoCSV/src/names.csv";
-
+        //storage
+        String linha = "";
+        //leitor de linhas
         BufferedReader br = new BufferedReader(new FileReader(path));
 
+        //ler até o fim do arquivo
+        while((linha = br.readLine()) != null){
+            System.out.println(linha);
+        }
     }
 }
